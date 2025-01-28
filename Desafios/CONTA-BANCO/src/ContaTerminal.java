5bb57e24789673a1d28eca906af75bca7526e81c
robsonjunior <robsonjunior.22@gmail.com>
2025-01-27 21:36:07 -0300

    Base do desafio

---
+++ Desafios/CONTA-BANCO/src/ContaTerminal.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    @SuppressWarnings("resource")
+    public static void main(String[] args) {
+
+        //TODO: Conhcer e importar a classe Scanner
+        
+        Scanner sc = new Scanner(System.in);
+
+        //Exibir as mensagens para o usuário
+
+        System.out.print("Digite seu nome: ");
+        String nome = sc.nextLine();
+
+        System.out.println("Olá, " + nome + "!");
+
+        //Obter pela Scanner os valores digitados no terminal
+
+        //Exibir a mensagem conta criada
+        
+    }
+}
